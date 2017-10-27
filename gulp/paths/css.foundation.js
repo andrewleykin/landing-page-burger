598576dab0a62a796133263b431965a7987e16cf
Лейкин Андрей Романович <Leykin.AR@kamaz.org>
2017-10-27 13:40:13 +0300

    last fix

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -3,5 +3,6 @@
 module.exports = [
   './node_modules/normalize.css/normalize.css',
   './node_modules/remodal/dist/remodal-default-theme.css',
-  './node_modules/remodal/dist/remodal.css'
+  './node_modules/remodal/dist/remodal.css',
+  './node_modules/slick-carousel/slick/slick.css'
 ];
